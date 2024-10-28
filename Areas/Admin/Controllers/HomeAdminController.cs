fbba43bb237b15ee792b3df51b8b1677b38aa8f0
nhdhieuu <22520439@gm.uit.edu.vn>
2024-10-28 14:30:02 +0700

    add Quan Li Nguoi Dung

--- Areas/Admin/Controllers/HomeAdminController.cs
+++ Areas/Admin/Controllers/HomeAdminController.cs
@@ -74,11 +74,75 @@ namespace ThiThucHanh.Areas.Admin.Controllers
         public IActionResult XoaSanPham(int maSanPham)
         {
             TempData["Message"] = "";
-            var chiTietSanPham = db.Products.Where(x => x.Id == maSanPham);
             db.Remove(db.Products.Find(maSanPham));
             db.SaveChanges();
             TempData["Message"] = "Sản phẩm đã được xóa";
             return RedirectToAction("index", "HomeAdmin");
+        }
+
+        [Route("danhmucnguoidung")]
+        [Authentication]
+        public IActionResult DanhMucNguoiDung(int? page)
+        {
+            int pageSize = 5;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            // Thêm điều kiện Where để lọc người dùng có Role = 1
+            var lstnguoidung = db.Users.AsNoTracking()
+                                      .Where(x => x.Role == 1)
+                                      .OrderBy(x => x.Id);
+            PagedList<User> lst = new PagedList<User>(lstnguoidung, pageNumber, pageSize);
+            return View(lst);
+        }
+
+        [Route("ThemNguoiDungMoi")]
+        [HttpGet]
+        public IActionResult ThemNguoiDungMoi()
+        {
+            return View();
+        }
+        [Route("ThemNguoiDungMoi")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ThemNguoiDungMoi(User nguoidung)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(nguoidung);
+                db.SaveChanges();
+                return RedirectToAction("danhmucnguoidung");
+            }
+            return View(nguoidung);
+        }
+        [Route("SuaNguoiDung")]
+        [HttpGet]
+        public IActionResult SuaNguoiDung(int maNguoiDung)
+        {
+            var nguoiDung = db.Users.Find(maNguoiDung);
+            return View(nguoiDung);
+
+        }
+        [Route("SuaNguoiDung")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SuaNguoiDung(User nguoiDung)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nguoiDung).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("danhmucnguoidung", "HomeAdmin");
+            }
+            return View(nguoiDung);
+        }
+        [Route("XoaNguoiDung")]
+        [HttpGet]
+        public IActionResult XoaNguoiDung(int maNguoiDung)
+        {
+            TempData["Message"] = "";
+            db.Remove(db.Users.Find(maNguoiDung));
+            db.SaveChanges();
+            TempData["Message"] = "User đã được xóa";
+            return RedirectToAction("danhmucnguoidung", "HomeAdmin");
         }
     }
 }
